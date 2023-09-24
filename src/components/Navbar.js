52f7b0e655881756f8e27cb226ef1599782f5c73
mahzabinrashid <mahzabinrashidfariha@gmail.com>
2023-09-24 04:45:51 -0400

    added smooth scroll to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
+import React, { useState } from "react";
 import "./Navbar.scss";
-import { useState } from "react";
 
 function Navbar() {
   const [isActive, setActive] = useState(false);
@@ -8,9 +8,23 @@ function Navbar() {
     setActive(!isActive);
   };
 
+  const scrollToSection = (sectionId) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      const navbarHeight = document.querySelector(".navbar").offsetHeight;
+      const sectionTop =
+        section.getBoundingClientRect().top + window.scrollY - navbarHeight;
+      window.scrollTo({ top: sectionTop, behavior: "smooth" });
+      // Close the mobile navigation menu if it's open
+      if (isActive) {
+        setActive(false);
+      }
+    }
+  };
+
   return (
     <nav className="navbar">
-      <div className="brand-title"> </div>
+      <div className="brand-title"></div>
       <div className="toggle-button" onClick={toggleNavbar}>
         <span className="bar"></span>
         <span className="bar"></span>
@@ -19,14 +33,19 @@ function Navbar() {
       <div className={isActive ? "active navbar-links" : "navbar-links"}>
         <ul>
           <li>
-            <a href="#experience"><span>01.</span> Experience</a>
+            <a onClick={() => scrollToSection("experience")}>
+              <span>01.</span> Experience
+            </a>
           </li>
           <li>
-            <a href="#projects"><span>02.</span> Projects</a>
+            <a onClick={() => scrollToSection("projects")}>
+              <span>02.</span> Projects
+            </a>
           </li>
-
           <li>
-            <a href="#contact"><span>03.</span> Contact</a>
+            <a onClick={() => scrollToSection("contact")}>
+              <span>03.</span> Contact
+            </a>
           </li>
         </ul>
       </div>
